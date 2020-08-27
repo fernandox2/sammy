9c11b3fa3011f7f0edd43477c218e74ec8600fde
fernandox2 <fernando.romero20@gmail.com>
2020-08-27 08:53:12 -0400

    Ultimo

--- public/js/app.js
+++ public/js/app.js
@@ -45977,7 +45977,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\carrito\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\sammy\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
